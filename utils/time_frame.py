ea7bfc1e26341b23454820777204de5c723caa76
Qubit47 <qubit47@gmail.com>
2020-05-31 01:41:50 +0800

    Update Code
    Signed-off-by: Qubit47 <qubit47@gmail.com>

--- utils/time_frame.py
+++ utils/time_frame.py
@@ -1,3 +1,3 @@
 # TODO:
 class TimeLineFrame:
-    def __init__(self, ally_unit:list[Unit], enemy_unit:list[Unit], union_burst):
+    def __init__(self , ally_unit:list[Unit], enemy_unit:list[Unit], union_burst):
